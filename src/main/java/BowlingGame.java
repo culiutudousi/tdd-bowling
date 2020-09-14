cbae66e3576c7a15fbb81ac73d653c954522ccf1
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-14 12:10:15 -0700

    refactor code

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -8,21 +8,25 @@ public class BowlingGame {
     public int calculateScore(int[] throwScores) {
         List<Integer> throwScoresList = Arrays.stream(throwScores).boxed().collect(Collectors.toList());
         List<List<Integer>> frames = splitScoresIntoFrames(throwScoresList);
-        return calculateScoreOfFrames(frames);
+        return calculateScoreOfFrames(frames).intValue();
     }
 
     private Integer calculateScoreOfFrames(List<List<Integer>> frames) {
         Integer score = 0;
-        if(frames.size() < 12) {
-            int numberOfEmptyToAdd = 12 - frames.size();
-            for (int i = 0; i < numberOfEmptyToAdd; i ++) {
-                frames.add(Arrays.asList(0));
-            }
-        }
+        lengthenFramesTo(frames, 12);
         for (int i = 0; i < 10; i ++) {
             score += calculateScoreOfFrame(frames.get(i), frames.get(i + 1), frames.get(i + 2));
         }
         return score;
+    }
+
+    private void lengthenFramesTo(List<List<Integer>> frames, Integer number) {
+        if(frames.size() < number) {
+            int numberOfEmptyToAdd = number - frames.size();
+            for (int i = 0; i < numberOfEmptyToAdd; i ++) {
+                frames.add(Arrays.asList(0));
+            }
+        }
     }
 
     private Integer calculateScoreOfFrame(List<Integer> frame, List<Integer> nextFrame, List<Integer> frameAfterNext) {
